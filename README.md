874c349e465cfcf570d4ff46f8e5a6a01915e6a2
Sami <puggsoy@gmail.com>
2017-01-08 22:05:59 +1300

    Added dialogue option and little message at the start

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 This is a tool to extract sprites from the data.win file of games made with Game Maker Studio.
 
 ## Usage
+You can either double-click on the executable to bring up dialogues to choose the input file and output directory, or run it via the command-line:
+
 	Usage: $pName inFile outDir [-s|-a]
 		inFile: The data.win file to extract from
 		outDir: The folder to save the extracted files to
 		-s: Only extract sprites
 		-a: Only extract audio
+
+Note that if run using the first method it will extract both sprites and audio.
 
 ## Compilation
 The code should be compiled as a Neko or C++ command-line program (only tested in Neko).
--- src/Main.hx
+++ src/Main.hx
@@ -3,20 +3,18 @@ package;
 import Util.*;
 import easyconsole.Begin;
 import easyconsole.End;
-import haxe.ds.Vector;
 import haxe.io.Path;
-import openfl.display.BitmapData;
 import structure.Sond;
 import structure.Sprt;
 import sys.FileSystem;
 import sys.io.File;
 import sys.io.FileInput;
 import sys.io.FileSeek;
+import systools.Dialogs;
 
 class Main
 {
-	private var txtrOff:Int;
-	private var sheets:Vector<BitmapData>;
+	private var inFile:String;
 	private var outDir:String;
 	
 	private var sprites:Bool = false;
@@ -29,16 +27,18 @@ class Main
 	
 	public function new() 
 	{
+		Sys.println('GMExtract v1.1 - by puggsoy\n');
+		
 		var pName:String = Path.withoutExtension(Path.withoutDirectory(Sys.executablePath()));
 		Begin.init();
-		Begin.usage = 'Usage: $pName inFile outDir [-s|-a]]\n    inFile: The data.win file to extract from\n    outDir: The folder to save the extracted files to\n    -s: Only extract sprites\n    -a: Only extract audio';
-		Begin.functions = [null, null, checkArgs];
+		Begin.usage = 'Usage: $pName inFile outDir [-s|-a]]\n    inFile: The data.win file to extract from\n    outDir: The folder to save the extracted files to\n    -s: Only extract sprites\n    -a: Only extract audio\n';
+		Begin.functions = [useUI, null, checkArgs];
 		Begin.parseArgs();
 	}
 	
 	private function checkArgs()
 	{
-		var inFile:String = Begin.args[0];
+		inFile = Begin.args[0];
 		
 		if (!FileSystem.exists(inFile))
 		{
@@ -57,7 +57,39 @@ class Main
 		outDir = Path.addTrailingSlash(outDir);
 		
 		//Start working
-		extract(inFile);
+		extract();
+		
+		End.anyKeyExit(0, "Done");
+	}
+	
+	private function useUI()
+	{
+		Sys.println(Begin.usage);
+		
+		var filters:FILEFILTERS =
+		{
+			count: 1,
+			descriptions: ['.win files'],
+			extensions: ['*.win']
+		};
+		
+		inFile = Dialogs.openFile('Select data.win file', 'Select data.win file', filters)[0];
+		
+		if (inFile == null)
+		{
+			End.terminate(1, 'No input file chosen!');
+		}
+		
+		outDir = Dialogs.folder('Select output directory...', 'Select output directory...');
+		
+		if (outDir == null)
+		{
+			End.terminate(1, 'No output directory chosen!');
+		}
+		
+		sprites = audio = true;
+		
+		extract();
 		
 		End.anyKeyExit(0, "Done");
 	}
@@ -79,9 +111,9 @@ class Main
 			sprites = audio = true;
 	}
 	
-	private function extract(fPath:String)
+	private function extract()
 	{
-		var f:FileInput = File.read(fPath);
+		var f:FileInput = File.read(inFile);
 		f.bigEndian = false;
 		f.seek(0, FileSeek.SeekEnd);
 		var fLen:Int = f.tell();
